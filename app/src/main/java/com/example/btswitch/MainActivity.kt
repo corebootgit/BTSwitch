977b2e9fa781ad863e6b56e68ccdcfd6439f48cc
marcin <coreboot@tlen.pl>
2019-07-13 22:51:32 +0200

    Check BT adapter...

--- app/src/main/java/com/example/btswitch/MainActivity.kt
+++ app/src/main/java/com/example/btswitch/MainActivity.kt
@@ -1,13 +1,44 @@
 package com.example.btswitch
 
+import android.app.ListActivity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_ENABLE_BT: Int = 1
+    val handler: Handler
+        get() {
+            TODO()
+        }
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private val BluetoothAdapter.isDisabled: Boolean
+        get() = !isEnabled
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        bluetoothAdapter?.takeIf { it.isDisabled }?.apply {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+
+        //BLEScan(bluetoothAdapter!!,  handler)
+
     }
+
+
 }
